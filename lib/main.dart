1e3989eaa45c9f57e778538ac4233ebcf479bf57
Сорокин Владимир <sorokin@it-apriori.ru>
2022-12-19 16:49:36 +0300

    ad sett page

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Accountant Calendar',
       theme: ThemeData(
-        
         primarySwatch: Colors.blue,
       ),
       home: const MainPage(),
     );
   }
 }
-
